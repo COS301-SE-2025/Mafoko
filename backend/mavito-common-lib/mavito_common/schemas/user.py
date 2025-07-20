5c23f86bdf262a006c0c5127ef62098f1b3737ed
eugenvosloo <u20445696@tuks.co.za>
2025-07-20 22:51:08 +0200

    feat: add profile picture endpoints

--- backend/mavito-common-lib/mavito_common/schemas/user.py
+++ backend/mavito-common-lib/mavito_common/schemas/user.py
@@ -35,8 +35,11 @@ class UserUpdate(BaseModel):  # Defined independently for clarity on updatable f
     role: Optional[UserRole] = None
     is_active: Optional[bool] = None
     is_verified: Optional[bool] = None
-    profile_pic_url: Optional[str] = None
     account_locked: Optional[bool] = None  # If admins can update this
+
+
+class UserProfilePictureUpdate(BaseModel):
+    profile_pic_url: str
 
 
 # Properties to return to client (this is your main User response model)
