5778fc38706fa7fa15cfae0a70b339c9f7ea3b41
ArnaudZStrydom <blizsandpluto@gmail.com>
2025-07-22 13:56:46 +0200

    Alembic  update

--- backend/alembic-service/entrypoint.sh
+++ backend/alembic-service/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-set -e
+set -e # Exit immediately if a command exits with a non-zero status
 
-echo "Installing common library..."
-pip install -e /mavito-common-lib-src
+echo "Starting entrypoint script for Alembic (Cloud Run compatible)..."
+
+
+# Debugging (optional, can be removed once successful)
+echo "Listing contents of /app/:"
+ls -l /app/
+echo "Displaying content of /app/migrations/alembic.ini:"
+cat /app/migrations/alembic.ini || { echo "Failed to read /app/migrations/alembic.ini, exiting."; exit 1; }
 
 # Execute the command (fallback to migrations if none provided)
 if [ "$#" -eq 0 ]; then
-    exec python -m alembic upgrade head
+    # Alembic.ini is at /app/migrations/alembic.ini
+    exec python -m alembic -c migrations/alembic.ini upgrade head
 else
     exec "$@"
 fi
