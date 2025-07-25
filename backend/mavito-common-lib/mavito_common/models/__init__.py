f3b4ed1eccbf95660984c86b21de0d4d59f257c1
Palesa-Thabane <thabanepalesa016@gmail.com>
2025-07-25 19:42:27 +0200

    feat : added _init_.py for models

--- backend/mavito-common-lib/mavito_common/models/__init__.py
+++ backend/mavito-common-lib/mavito_common/models/__init__.py
@@ -3,3 +3,11 @@ from .user import User  # noqa: F401
 from .linguist_application import LinguistApplication  # noqa: F401
 from .term import Term  # noqa: F401
 from .term_vote import TermVote  # noqa: F401
+from .workspace import (  # noqa: F401
+    BookmarkedTerm,
+    BookmarkedGlossary,
+    WorkspaceGroup,
+    WorkspaceGroupItem,
+    GroupType,
+    ItemType,
+)
