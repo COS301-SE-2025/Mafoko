4870e888ad3b218bc1eb857dd196e8947e96587b
Zainab <abdulrasaqzainab0@gmail.com>
2025-08-01 11:48:03 +0200

    Revert "Merge branch 'Feature/glossary_styles' into documents/workspace"
    This reverts commit 605b1e925dc9a1a83a80f8f5bea5532a27afdc25, reversing
    changes made to 78b817f6791ce18c511d71f069653f83a6037944.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,11 +12,8 @@ import CommunityHelpPage from './pages/help/CommunityHelp.tsx';
 import TermHelpPage from './pages/help/TermHelpPage.tsx';
 import FrequentlyAskedPage from './pages/help/FrequentlyAskedPage.tsx';
 import AdminPage from './pages/AdminPage.tsx';
-
+import GlossaryPage from './pages/GlossaryPage.tsx';
 import WorkspacePage from './pages/WorkspacePage.tsx';
-
-import NewGlossary from './pages/NewGlossary';
-
 import { TermDetailPage } from './pages/TermDetailPage.tsx';
 import './App.css';
 
@@ -67,11 +64,8 @@ function App() {
         <Route path="/help/terms" element={<TermHelpPage />} />
         <Route path="/help/faqs" element={<FrequentlyAskedPage />} />
         <Route path="/admin" element={<AdminPage />} />
-
+        <Route path="/glossary" element={<GlossaryPage />} />
         <Route path="/workspace" element={<WorkspacePage />} />
-
-  <Route path="/glossary" element={<NewGlossary />} />
- Feature/glossary_styles
         <Route path="/term/:termId" element={<TermDetailPage />} />
 
       </Routes>
