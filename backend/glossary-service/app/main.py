da79110adfb62ef1fa791d4c54113c48cc771ff9
hayley-d <hayley@codehesion.com>
2025-10-08 08:41:44 +0200

    Fix tests

--- backend/glossary-service/app/main.py
+++ backend/glossary-service/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from mavito_common.core.config import settings
 from app.api.v1.endpoints import glossary
 
 app = FastAPI(title="Marito Search Service")
