01aa1b916e05a26bff125fc0a898001202ab79dd
hayley-d <hayley@codehesion.com>
2025-10-08 08:45:19 +0200

    Fix tests

--- backend/glossary-service/app/main.py
+++ backend/glossary-service/app/main.py
@@ -1,21 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from mavito_common.core.config import settings
 from app.api.v1.endpoints import glossary
 
 app = FastAPI(title="Marito Search Service")
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins, 
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+if settings.BACKEND_CORS_ORIGINS_LIST:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS_LIST],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 app.include_router(glossary.router, prefix="/api/v1/glossary", tags=["Glossary"])
 
