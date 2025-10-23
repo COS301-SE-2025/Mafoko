e05fc435cca75afae9a1a715a38eaac571eb84da
ArnaudZStrydom <blizsandpluto@gmail.com>
2025-10-23 15:31:18 +0200

    Final learning path UI changes

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -31,6 +31,7 @@ export default tseslint.config(
       'src/utils/syncManager.ts',
       'src/utils/cacheUpdater.ts',
       'src/utils/exportUtils.ts',
+      'src/pages/LearningPathPage.tsx',
     ],
   },
 
